cfdb3c543869ccbeb5eed33aae5fa371427ae379
Ubuntu <ubuntu@dl-poly.novalocal>
2016-04-20 05:21:43 +0000

    fixed errors

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install gfortran g++
+sudo apt-get -y  install gfortran g++
 
 sudo apt-get -y install git
 sudo apt-get -y install xauth x11-apps
@@ -9,9 +9,11 @@ sudo apt-get -y install xemacs21 xemacs21-supportel vim
 sudo apt-get -y install bc wget make
 sudo apt-get -y install build-essential flex bison zlib1g-dev 
 sudo apt-get -y install libboost-system-dev libboost-thread-dev 
-sudo apt-get -y libopenmpi-dev openmpi-bin gnuplot librealine-dev 
-sudo apt-get -y libncurses-dev libxt-dev
+sudo apt-get -y install libopenmpi-dev openmpi-bin gnuplot librealine-dev 
+sudo apt-get -y install libncurses-dev libxt-dev
 sudo apt-get -y install environment-modules
+sudo apt-get -y install zip
+
 
 # change the ownership to allow access to ephemeral storage on 
 # on your instance
