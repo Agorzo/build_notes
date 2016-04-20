37bca70618653dfb41025e5b96168a4ea241da96
kofi ofori <kofi.ofori@postgrad.curtin.edu.au>
2016-04-20 04:07:42 +0000

    build script for a new instance prepared for dl_poly

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install gfortran g++
+
+sudo apt-get -y install git
+sudo apt-get -y install xauth x11-apps
+sudo apt-get -y install xemacs21 xemacs21-supportel vim
+sudo apt-get -y install bc wget make
+sudo apt-get -y install build-essential flex bison zlib1g-dev 
+sudo apt-get -y install libboost-system-dev libboost-thread-dev 
+sudo apt-get -y libopenmpi-dev openmpi-bin gnuplot librealine-dev 
+sudo apt-get -y libncurses-dev libxt-dev
+sudo apt-get -y install environment-modules
+
+# change the ownership to allow access to ephemeral storage on 
+# on your instance
+
+sudo chown ubuntu /mnt
+
